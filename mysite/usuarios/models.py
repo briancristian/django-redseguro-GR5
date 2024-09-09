415376420f05a6cd17199820f62267922ad42062
fredonra <fede_99_leiva@hotmail.com>
2024-09-09 19:36:31 -0300

    feat: agrego key unique=True a la propiedad email del model Usuario

--- mysite/usuarios/models.py
+++ mysite/usuarios/models.py
@@ -5,7 +5,7 @@ class Usuario(models.Model):
     usuario_id = models.AutoField(primary_key=True)
     nombre = models.CharField(null=False, max_length=50)
     apellido = models.CharField(null=False, max_length=50)
-    mail = models.EmailField(null=False)
+    mail = models.EmailField(null=False, unique=True)
     
     def __str__(self):
         return f'{self.nombre} {self.apellido}'
